46bf76ee7445923d2c2b134e262ecf6384c422ba
Bo Zhang <bo@gradle.com>
2020-05-19 16:57:49 +0800

    Fix more issues for distributed test
    We found several issues breaking distributed test:
    
    - ScriptExecuter not using same java version.
    - Task depends on tooling api fat jar but not declares inputs.
    
    This commit fixes these issues.

--- buildSrc/subprojects/integration-testing/src/main/kotlin/org/gradle/gradlebuild/test/integrationtests/shared-configuration.kt
+++ buildSrc/subprojects/integration-testing/src/main/kotlin/org/gradle/gradlebuild/test/integrationtests/shared-configuration.kt
@@ -4,11 +4,13 @@ import accessors.groovy
 import accessors.java
 import org.gradle.api.Action
 import org.gradle.api.Project
+import org.gradle.api.tasks.ClasspathNormalizer
 import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.SourceSet
 import org.gradle.api.tasks.TaskProvider
 import org.gradle.kotlin.dsl.*
 import org.gradle.plugins.ide.idea.IdeaPlugin
+import org.gradle.gradlebuild.versioning.buildVersion
 
 
 enum class TestType(val prefix: String, val executers: List<String>, val libRepoRequired: Boolean) {
@@ -100,6 +102,25 @@ fun Project.integrationTestUsesSampleDir(vararg sampleDirs: String) {
         inputs.files(rootProject.files(sampleDirs))
             .withPropertyName("autoTestedSamples")
             .withPathSensitivity(PathSensitivity.RELATIVE)
+    }
+}
+
+
+fun Project.integrationTestUsesKotlinDslPlugins() {
+    tasks.withType<IntegrationTest>().configureEach {
+        inputs.dir(project(":kotlinDslPlugins").buildDir.resolve("repository"))
+            .withPropertyName("localRepository")
+            .withPathSensitivity(PathSensitivity.RELATIVE)
+    }
+}
+
+
+fun Project.integrationTestUsesToolingApiFatJar() {
+    tasks.withType<IntegrationTest>().configureEach {
+        inputs.file(project(":toolingApi").buildDir.resolve("shaded-jar/gradle-tooling-api-shaded-${rootProject.buildVersion.baseVersion}.jar"))
+            .withPropertyName("fatToolingApiJar")
+            .withPathSensitivity(PathSensitivity.RELATIVE)
+            .withNormalizer(ClasspathNormalizer::class)
     }
 }
 
